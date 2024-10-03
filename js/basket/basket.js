99a6aa724d1adaf34137a05e69edf25272424a40
Oleksii03 <102686871+Oleksii03@users.noreply.github.com>
2024-10-03 14:52:05 +0000

    Deploying to gh-pages from @ Oleksii03/Flowers@da1790985af2e92604dc5a76ebb73bc1da41e6b5 🚀

--- js/basket/basket.js
+++ js/basket/basket.js
@@ -1,5 +1,5 @@
-import{K as k,u as h,h as y,m as S,c as j}from"../../assets/update-local-storage-counter-CAwkdD6p.js";function $(l){return l.map((_,d)=>{const{id:a,discount:r,price:s,oldPrice:p,title:b,img:{png:u,webp:g}}=_;return`
-      <li class="basket-main__list-item" data-id="${a}">
+import{K as k,u as h,g as S,f as y,h as j,m as $,c as w,r as q}from"../../assets/mixins-jrSAJQO9.js";import{g as L}from"../../assets/vendor-CWWLBHNg.js";function P(a){return a.map((m,r)=>{const{id:c,discount:o,price:t,oldPrice:l,title:u,img:{png:_,webp:g}}=m;return`
+      <li class="basket-main__list-item" data-id="${c}">
           <picture class="basket-main__list-picture">
             <source
               srcset="${g}"
@@ -8,16 +8,16 @@ import{K as k,u as h,h as y,m as S,c as j}from"../../assets/update-local-storage
               class="basket-main__list-img"
               width="83"
               height="104"
-              src="${u}"
+              src="${_}"
               alt="#" />
           </picture>
 
           <div class="basket-main__list-info">
-            <h3 class="basket-main__list-title">${b}</h3>
+            <h3 class="basket-main__list-title">${u}</h3>
 
             <div class="basket-main__list-price-piece">
-              <p class="basket-main__list-price-piece_new">${s} <span>₴</span></p>
-              ${r?`<p class="basket-main__list-price-piece_old js-old-price" data-old-price="${p}">${p} <span>₴</span></p>`:""}
+              <p class="basket-main__list-price-piece_new">${t} <span>₴</span></p>
+              ${o?`<p class="basket-main__list-price-piece_old js-old-price" data-old-price="${l}">${l} <span>₴</span></p>`:""}
             </div>
 
             <div class="basket-main__list-amount-box">
@@ -57,11 +57,11 @@ import{K as k,u as h,h as y,m as S,c as j}from"../../assets/update-local-storage
               </svg>
             </div>
             <p class="list-price__price js-current-price" 
-            data-price="${s}">
-            ${s}<span> ₴</span>
+            data-price="${t}">
+            ${t}<span> ₴</span>
             </p>
           </div>
-        </li>`})}function v(){return`
+        </li>`})}function C(){return`
       <li
         class="basket-main__list-item_empty-basket list-item__empty-basket js-empty-basket-item">
         <h3 class="list-item__empty-basket-title">
@@ -73,5 +73,5 @@ import{K as k,u as h,h as y,m as S,c as j}from"../../assets/update-local-storage
           height="500"
           src="https://firebasestorage.googleapis.com/v0/b/flowers-4b1a5.appspot.com/o/pages%2F404%2Fempty-shopping-cart.webp?alt=media&token=3c262b31-1194-4d5e-9cc7-4c1f827f99a0"
           alt="#" />
-      </li>`}function q(){const l=document.querySelector(".js-basket-card-list"),_=document.querySelector(".js-title-shopping-list"),d=document.querySelector(".js-order-total-price"),a=document.querySelector(".js-goods-price"),r=document.querySelector(".js-discount-price");let s=JSON.parse(localStorage.getItem(k))??[];function p(t){t.length?(l.innerHTML=$(t).join(""),_.classList.remove("basket-main__continue-shopping-list_hidden")):(l.innerHTML=v(),_.classList.add("basket-main__continue-shopping-list_hidden"))}p(s),l.addEventListener("click",b);function b({target:t,type:e}){t.classList.contains("js-remove-card")&&x(t),t.closest(".js-basket-btn-increment")?g(t):t.closest(".js-basket-btn-decrement")&&C(t)}function u(t){let e=0,n=0;t.forEach(i=>{e+=i.oldPrice,n+=i.price}),d.textContent=`${n} ₴`,a.textContent=`${e} ₴`,r.textContent=`${e-n} ₴`}u(s);function g(t){const e=t.closest(".basket-main__list-item");let n=e.querySelector(".js-basket-amount"),i=e.querySelector(".js-current-price"),m=e.querySelector(".js-old-price")?e.querySelector(".js-old-price").dataset.oldPrice:+i.dataset.price,c=+n.textContent;const o=+i.dataset.price;c>=10||(n.textContent=c+=1,i.textContent=`${o*c} ₴`,a.textContent=`${parseInt(a.textContent)+Number(m)} ₴`,d.textContent=`${parseInt(d.textContent)+o} ₴`,r.textContent=`${parseInt(r.textContent)+(Number(m)-o)} ₴`)}function C(t){const e=t.closest(".basket-main__list-item");let n=e.querySelector(".js-basket-amount"),i=e.querySelector(".js-current-price"),m=e.querySelector(".js-old-price")?e.querySelector(".js-old-price").dataset.oldPrice:+i.dataset.price,c=+n.textContent;const o=+i.dataset.price;c<=1||(n.textContent=c-=1,i.textContent=`${o*c} ₴`,a.textContent=`${parseInt(a.textContent)-Number(m)} ₴`,d.textContent=`${parseInt(d.textContent)-o} ₴`,r.textContent=`${parseInt(r.textContent)-(Number(m)-o)} ₴`)}function x(t){const e=t.closest(".basket-main__list-item"),n=e.dataset.id;e.remove(),s=s.filter(({id:i})=>i!==n),localStorage.setItem(k,JSON.stringify(s)),h(),u(s),f(),s.length||(l.innerHTML=v())}window.addEventListener("storage",f);function f(){s=JSON.parse(localStorage.getItem(k))??[],h(),p(s),u(s)}}document.addEventListener("DOMContentLoaded",()=>{y(),S(),j(),q(),h()});
+      </li>`}function E(){const a=document.querySelector(".js-basket-card-list"),m=document.querySelector(".js-title-shopping-list"),r=document.querySelector(".js-order-total-price"),c=document.querySelector(".js-goods-price"),o=document.querySelector(".js-discount-price");let t=JSON.parse(localStorage.getItem(k))??[];function l(e){e.length?(a.innerHTML=P(e).join(""),m.classList.remove("basket-main__continue-shopping-list_hidden")):(a.innerHTML=C(),m.classList.add("basket-main__continue-shopping-list_hidden"))}l(t),a.addEventListener("click",u);function u({target:e,type:s}){e.classList.contains("js-remove-card")&&x(e),e.closest(".js-basket-btn-increment")?g(e):e.closest(".js-basket-btn-decrement")&&v(e)}function _(e){let s=0,n=0;e.forEach(i=>{s+=i.oldPrice,n+=i.price}),r.textContent=`${n} ₴`,c.textContent=`${s} ₴`,o.textContent=`${s-n} ₴`}_(t);function g(e){const s=e.closest(".basket-main__list-item");let n=s.querySelector(".js-basket-amount"),i=s.querySelector(".js-current-price"),b=s.querySelector(".js-old-price")?s.querySelector(".js-old-price").dataset.oldPrice:+i.dataset.price,d=+n.textContent;const p=+i.dataset.price;d>=10||(n.textContent=d+=1,i.textContent=`${p*d} ₴`,c.textContent=`${parseInt(c.textContent)+Number(b)} ₴`,r.textContent=`${parseInt(r.textContent)+p} ₴`,o.textContent=`${parseInt(o.textContent)+(Number(b)-p)} ₴`)}function v(e){const s=e.closest(".basket-main__list-item");let n=s.querySelector(".js-basket-amount"),i=s.querySelector(".js-current-price"),b=s.querySelector(".js-old-price")?s.querySelector(".js-old-price").dataset.oldPrice:+i.dataset.price,d=+n.textContent;const p=+i.dataset.price;d<=1||(n.textContent=d-=1,i.textContent=`${p*d} ₴`,c.textContent=`${parseInt(c.textContent)-Number(b)} ₴`,r.textContent=`${parseInt(r.textContent)-p} ₴`,o.textContent=`${parseInt(o.textContent)-(Number(b)-p)} ₴`)}function x(e){const s=e.closest(".basket-main__list-item"),n=s.dataset.id;s.remove(),t=t.filter(({id:i})=>i!==n),localStorage.setItem(k,JSON.stringify(t)),h(),_(t),f(),t.length||(a.innerHTML=C())}window.addEventListener("storage",f);function f(){t=JSON.parse(localStorage.getItem(k))??[],h(),l(t),_(t)}}function I(a){const m=document.querySelector(".js-basket-recommendations-swiper"),r=["Recommendations"],c=[m],o=L(a);for(let t=0;t<r.length;t++){const l=r[t],u=c[t];S(o,l,u)}}document.addEventListener("DOMContentLoaded",()=>{let a=y();j(),$(),w(),E(),h(),q(),I(a)});
 //# sourceMappingURL=basket.js.map
--- js/index.js
+++ js/index.js
@@ -1,117 +1,4 @@
-import{K as g,a as f,u,h as S,m as y,c as j}from"../assets/update-local-storage-counter-CAwkdD6p.js";import{S as p,P as E,A as L,G as x,N as $,g as b,a as h,c as k,i as P}from"../assets/vendor-ANkZcBYd.js";p.use([E,L,x,$]);function q(){return new p(".mainHeroSwiper",{autoplay:{delay:2e3,disableOnInteraction:!0},pagination:{el:".swiper-pagination"},speed:600})}function I(){const e=new p(".stock-swiper",{slidesPerView:"auto",spaceBetween:9,grid:{rows:2,fill:"row"},pagination:{el:".swiper-pagination"},navigation:{nextEl:".stock__swiper-button-next",prevEl:".stock__swiper-button-prev"}});d(e),window.addEventListener("resize",()=>d(e))}function F(){const e=new p(".seasona-swiper",{slidesPerView:"auto",spaceBetween:9,grid:{rows:2,fill:"row"},pagination:{el:".swiper-pagination"},navigation:{nextEl:".seasona__swiper-button-next",prevEl:".seasona__swiper-button-prev"}});d(e),window.addEventListener("resize",()=>d(e))}function A(){const e=new p(".present-swiper",{slidesPerView:"auto",spaceBetween:9,grid:{rows:2,fill:"row"},pagination:{el:".swiper-pagination"},navigation:{nextEl:".present__swiper-button-next",prevEl:".present__swiper-button-prev"}});d(e),window.addEventListener("resize",()=>d(e))}function C(){return new p(".forum-swiper",{slidesPerView:"auto",spaceBetween:20,pagination:{el:".swiper-pagination"},speed:600})}function d(e){window.innerWidth<=425?(e.params.slidesPerView=2,e.params.spaceBetween=9):window.innerWidth>=1024?(e.params.spaceBetween=20,e.params.slidesPerView=4):e.params.slidesPerView="auto",e.update()}function O(e,t){const i=e.map(({id:r,discount:o,price:c,img:{png:s,webp:a},title:n})=>`
-          <div class="swiper-slide js-main-swiper-slide" data-id="${r}">
-            <div class="main__slide-stock slide-stock">
-              <div class="slide-stock__content-top">
-                <svg
-                  class="slide-stock__content-top-favorite js-favorite"
-                  width="33"
-                  height="28">
-                  <use xlink:href="#icon-heart"></use>
-                </svg>
-
-                ${o?`<div class="slide-stock__content-top-discount">-${o}%</div>`:""}
-             
-                  <picture>
-                    <source 
-                      srcset=${a?`${a}`:"https://firebasestorage.googleapis.com/v0/b/flowers-4b1a5.appspot.com/o/pages%2F404%2Fplaceholder-small-404.webp?alt=media&token=76b2ee6e-35c7-4f93-855a-01e009d82bea"} type="image/webp" />
-
-                    <img 
-                     loading="lazy"
-                     width="450"
-                     height="450"
-                     class="slide-stock__content-top-img js-main-swiper-slide-img"
-                     src=${s?`${s}`:"https://firebasestorage.googleapis.com/v0/b/flowers-4b1a5.appspot.com/o/pages%2F404%2Fplaceholder-small-404.png?alt=media&token=f6fac92f-15df-4da5-a695-b57533957121"} alt="${n}"
-                     data-src="${a}" />
-                  </picture>
-
-                <div class="slide-stock__content-top-logo">TOP</div>
-              </div>
-
-              <!-- content-bottom -->
-
-              <div class="slide-stock__content-bottom">
-                <ul class="slide-stock__content-bottom-rating-list">
-                  <li>
-                    <svg
-                      class="slide-stock__content-bottom-list-star"
-                      width="13"
-                      height="13">
-                      <use xlink:href="#star-rating"></use>
-                    </svg>
-                  </li>
-                  <li>
-                    <svg
-                      class="slide-stock__content-bottom-list-star"
-                      width="13"
-                      height="13">
-                      <use xlink:href="#star-rating"></use>
-                    </svg>
-                  </li>
-                  <li>
-                    <svg
-                      class="slide-stock__content-bottom-list-star"
-                      width="13"
-                      height="13">
-                      <use xlink:href="#star-rating"></use>
-                    </svg>
-                  </li>
-                  <li>
-                    <svg
-                      class="slide-stock__content-bottom-list-star"
-                      width="13"
-                      height="13">
-                      <use xlink:href="#star-rating"></use>
-                    </svg>
-                  </li>
-                  <li>
-                    <svg
-                      class="slide-stock__content-bottom-list-star"
-                      width="13"
-                      height="13">
-                      <use xlink:href="#star-rating"></use>
-                    </svg>
-                  </li>
-                </ul>
-
-                <div class="slide-stock__content-bottom-price-box">
-                  <h5 class="slide-stock__content-bottom-title js-main-swiper-slide-title" title="${n}">
-                    <span></span> ${n}
-                  </h5>
-
-                  <div class="slide-stock__content-bottom-price">
-
-                    ${o?`<div class="slide-stock__content-bottom-price-old js-main-swiper-slide-price-old"><span>${c}</span> грн</div>`:""}
-                    
-                    <div class="slide-stock__content-bottom-price-new   
-                      js-main-swiper-slide-price-new" 
-                      data-discount="${o}">
-                      ${c*(1-o/100).toFixed(1)} грн
-                    </div>
-                  </div>
-                </div>
-
-                <div class="slide-stock__content-bottom-buy">
-                  <a
-                    class="slide-stock__content-bottom-buy-link"
-                    href="#"
-                    target="_blank"
-                    ><span class="slide-stock__content-bottom-buy-link_mob">Замовити</span>
-                    <span class="slide-stock__content-bottom-buy-link_desc">Швидке замовлення</span>
-                  </a>
-
-                  <button class="slide-stock__content-bottom-buy-btn js-basket">
-                    <span>Замовити</span>
-
-                    <svg
-                      width="21"
-                      height="21">
-                      <use xlink:href="#icon-basket"></use>
-                    </svg>
-                  </button>
-                </div>
-              </div>
-            </div>
-          </div>`);t.innerHTML=i.join("")}function B(e){const t=document.querySelector(".js-main-swiper-stock"),i=document.querySelector(".js-main-swiper-seasonal"),r=document.querySelector(".js-main-swiper-present"),o=["Stock","Seasonal","Present"],c=[t,i,r],s=b(e);async function a(n="",l=""){try{const m=await h(k(s,n)),w=[];m.forEach(_=>{w.push({id:_.id,..._.data()})}),O(w,l)}catch(m){return console.error("Error getting documents:",m),[]}}for(let n=0;n<o.length;n++){const l=o[n],m=c[n];a(l,m)}}function N(){const e=document.querySelector(".js-preloader");t();function t(){if(!localStorage.getItem("is-visit")){i();return}e.classList.add("preloader_hidden"),document.body.classList.remove("to-freeze")}function i(){e.classList.add("preloader_visible"),document.body.classList.add("to-freeze"),setTimeout(()=>{e.classList.add("preloader_hidden"),document.body.classList.remove("to-freeze"),localStorage.setItem("is-visit","true")},4e3)}}function V(){return P({apiKey:"AIzaSyCeoo8r7oEqKQs1O_YxVO2RAd8WWV3HWso",authDomain:"flowers-4b1a5.firebaseapp.com",projectId:"flowers-4b1a5",storageBucket:"flowers-4b1a5.appspot.com",messagingSenderId:"719757325067",appId:"1:719757325067:web:6e609ba7f0d37bdd23e80e"})}function z(e){const t=document.querySelector(".js-main-swiper-forum"),i=e.map(({title:r,description:o,img:{png:c,webp:s}})=>`
+import{g,K as m,a as u,b as p,u as d,f as v,h as S,m as _,i as w,s as y,d as b,p as h,e as L,c as k}from"../assets/mixins-jrSAJQO9.js";import{g as f,a as E,c as j}from"../assets/vendor-CWWLBHNg.js";function q(s){const a=document.querySelector(".js-main-swiper-stock"),o=document.querySelector(".js-main-swiper-seasonal"),i=document.querySelector(".js-main-swiper-present"),r=["Stock","Seasonal","Present"],c=[a,o,i],e=f(s);for(let t=0;t<r.length;t++){const n=r[t],l=c[t];g(e,n,l)}}function I(){const s=document.querySelector(".js-preloader");a();function a(){if(!localStorage.getItem("is-visit")){o();return}s.classList.add("preloader_hidden"),document.body.classList.remove("to-freeze")}function o(){s.classList.add("preloader_visible"),document.body.classList.add("to-freeze"),setTimeout(()=>{s.classList.add("preloader_hidden"),document.body.classList.remove("to-freeze"),localStorage.setItem("is-visit","true")},4e3)}}function C(s){const a=document.querySelector(".js-main-swiper-forum"),o=s.map(({title:i,description:r,img:{png:c,webp:e}})=>`
       <div class="swiper-slide">
             <div class="main__slide-forum">
               <div class="main__slide-forum-picture-wrapper">
@@ -119,7 +6,7 @@ import{K as g,a as f,u,h as S,m as y,c as j}from"../assets/update-local-storage-
                   <div class="main__slide-forum-img-overlay"></div>
                   <picture class="main__slide-forum-picture">
                     <source
-                      srcset=${s?`${s}`:"https://firebasestorage.googleapis.com/v0/b/flowers-4b1a5.appspot.com/o/pages%2F404%2Fplaceholder-404.webp?alt=media&token=89f10487-08e7-491f-8448-5998d2423abc"}
+                      srcset=${e?`${e}`:"https://firebasestorage.googleapis.com/v0/b/flowers-4b1a5.appspot.com/o/pages%2F404%2Fplaceholder-404.webp?alt=media&token=89f10487-08e7-491f-8448-5998d2423abc"}
                       type="image/webp" />
 
                     <img
@@ -128,16 +15,16 @@ import{K as g,a as f,u,h as S,m as y,c as j}from"../assets/update-local-storage-
                       height="343"
                       loading="lazy"
                       src=${c?`${c}`:"https://firebasestorage.googleapis.com/v0/b/flowers-4b1a5.appspot.com/o/pages%2F404%2Fplaceholder-404.png?alt=media&token=69e0e0b2-fd05-421a-a13e-ce96a894acc6"}
-                      alt="${r}" />
+                      alt="${i}" />
                   </picture>
                 </div>
               </div>
 
               <div class="main__slide-forum-text">
-                <p class="main__slide-forum-title">${r}</p>
+                <p class="main__slide-forum-title">${i}</p>
 
-                <p class="main__slide-forum-description">${o}</p>
+                <p class="main__slide-forum-description">${r}</p>
               </div>
             </div>
-          </div>`);t.innerHTML=i.join("")}function T(e){const t=b(e);async function i(){try{const r=await h(k(t,"main-forum")),o=[];r.forEach(c=>{o.push({id:c.id,...c.data()})}),z(o)}catch(r){return console.error("Error getting documents:",r),[]}}i()}function v(e){const t=e.closest(".js-main-swiper-slide"),i=t.querySelector(".js-main-swiper-slide-title").textContent.trim(),r=t.querySelector(".js-main-swiper-slide-img").src,o=t.querySelector(".js-main-swiper-slide-img").dataset.src,c=t.querySelector(".js-main-swiper-slide-price-old")?t.querySelector(".js-main-swiper-slide-price-old").textContent.trim():0,s=t.querySelector(".js-main-swiper-slide-price-new").textContent.trim(),a=t.querySelector(".js-main-swiper-slide-price-new").dataset.discount;return{id:t.dataset.id,title:i,img:{png:r||"https://firebasestorage.googleapis.com/v0/b/flowers-4b1a5.appspot.com/o/pages%2F404%2Fplaceholder-small-404.png?alt=media&token=f6fac92f-15df-4da5-a695-b57533957121",webp:o||"https://firebasestorage.googleapis.com/v0/b/flowers-4b1a5.appspot.com/o/pages%2F404%2Fplaceholder-small-404.webp?alt=media&token=76b2ee6e-35c7-4f93-855a-01e009d82bea"},price:parseInt(s),oldPrice:parseInt(c)?parseInt(c):parseInt(s),discount:Number(a)}}function D(){const e=document.querySelector(".js-main-container");let t=JSON.parse(localStorage.getItem(g))??[],i=JSON.parse(localStorage.getItem(f))??[];e.addEventListener("click",r);function r({target:s}){s.classList.contains("js-basket")&&o(s),(s.classList.contains("js-favorite")||s.tagName==="use")&&c(s)}function o(s){const a=v(s);t.some(({id:l})=>l===a.id)||(t.push(a),localStorage.setItem(g,JSON.stringify(t)),u())}function c(s){let a=s.closest(".js-favorite");if(a)if(a.classList.contains("slide-stock__content-top-favorite_active")){a.classList.remove("slide-stock__content-top-favorite_active");const n=a.closest(".js-main-swiper-slide").dataset.id;i=i.filter(({id:l})=>l!==n),localStorage.setItem(f,JSON.stringify(i)),u()}else{a.classList.add("slide-stock__content-top-favorite_active");const n=v(s);i.push(n),localStorage.setItem(f,JSON.stringify(i)),u()}}u(),addEventListener("load",()=>{const s=document.querySelectorAll(".js-main-swiper-slide");i.forEach(({id:a})=>{[...s].forEach(n=>{n.dataset.id===a&&n.querySelector(".js-favorite").classList.add("slide-stock__content-top-favorite_active")})})}),window.addEventListener("storage",()=>{t=JSON.parse(localStorage.getItem(g))??[],u()})}document.addEventListener("DOMContentLoaded",()=>{let e=V();S(),y(),q(),B(e),I(),N(),F(),A(),C(),T(e),j(),D()});
+          </div>`);a.innerHTML=o.join("")}function F(s){const a=f(s);async function o(){try{const i=await E(j(a,"main-forum")),r=[];i.forEach(c=>{r.push({id:c.id,...c.data()})}),C(r)}catch(i){return console.error("Error getting documents:",i),[]}}o()}function O(){const s=document.querySelector(".js-main-container");let a=JSON.parse(localStorage.getItem(m))??[],o=JSON.parse(localStorage.getItem(u))??[];s.addEventListener("click",i);function i({target:e}){e.classList.contains("js-basket")&&r(e),(e.classList.contains("js-favorite")||e.tagName==="use")&&c(e)}function r(e){const t=p(e);a.some(({id:l})=>l===t.id)||(a.push(t),localStorage.setItem(m,JSON.stringify(a)),d())}function c(e){let t=e.closest(".js-favorite");if(t)if(t.classList.contains("slide-stock__content-top-favorite_active")){t.classList.remove("slide-stock__content-top-favorite_active");const n=t.closest(".js-main-swiper-slide").dataset.id;o=o.filter(({id:l})=>l!==n),localStorage.setItem(u,JSON.stringify(o)),d()}else{t.classList.add("slide-stock__content-top-favorite_active");const n=p(e);o.push(n),localStorage.setItem(u,JSON.stringify(o)),d()}}d(),addEventListener("load",()=>{const e=document.querySelectorAll(".js-main-swiper-slide");o.forEach(({id:t})=>{[...e].forEach(n=>{n.dataset.id===t&&n.querySelector(".js-favorite").classList.add("slide-stock__content-top-favorite_active")})})}),window.addEventListener("storage",()=>{a=JSON.parse(localStorage.getItem(m))??[],d()})}document.addEventListener("DOMContentLoaded",()=>{let s=v();S(),_(),w(),q(s),y(),I(),b(),h(),L(),F(s),k(),O()});
 //# sourceMappingURL=index.js.map
