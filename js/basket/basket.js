0316c2f9bec470399469852f6a68541ad88b97ca
Oleksii03 <102686871+Oleksii03@users.noreply.github.com>
2024-10-07 13:24:13 +0000

    Deploying to gh-pages from @ Oleksii03/Flowers@16e52172f98ddd56c647dc07d2ecc57820b9f2a3 🚀

--- js/basket/basket.js
+++ js/basket/basket.js
@@ -1,23 +1,23 @@
-import{K as k,u as h,g as S,f as y,h as j,m as $,c as w,r as q}from"../../assets/mixins-BXRfE859.js";import{g as L}from"../../assets/vendor-CWWLBHNg.js";function P(a){return a.map((m,r)=>{const{id:c,discount:o,price:t,oldPrice:l,title:u,img:{png:_,webp:g}}=m;return`
+import{K as g,u as k,g as y,f as j,h as $,m as q,c as w,r as L}from"../../assets/mixins-BXRfE859.js";import{g as P}from"../../assets/vendor-CWWLBHNg.js";function E(r){return r.map((u,o)=>{const{id:c,discount:l,price:n,oldPrice:e,title:d,img:{png:h,webp:_}}=u;return`
       <li class="basket-main__list-item" data-id="${c}">
           <picture class="basket-main__list-picture">
             <source
-              srcset="${g}"
+              srcset="${_}"
               type="image/webp" />
             <img
               class="basket-main__list-img"
               width="83"
               height="104"
-              src="${_}"
+              src="${h}"
               alt="#" />
           </picture>
 
           <div class="basket-main__list-info">
-            <h3 class="basket-main__list-title">${u}</h3>
+            <h3 class="basket-main__list-title">${d}</h3>
 
             <div class="basket-main__list-price-piece">
-              <p class="basket-main__list-price-piece_new">${t} <span>₴</span></p>
-              ${o?`<p class="basket-main__list-price-piece_old js-old-price" data-old-price="${l}">${l} <span>₴</span></p>`:""}
+              <p class="basket-main__list-price-piece_new">${n} <span>₴</span></p>
+              ${l?`<p class="basket-main__list-price-piece_old js-old-price" data-old-price="${e}">${e} <span>₴</span></p>`:""}
             </div>
 
             <div class="basket-main__list-amount-box">
@@ -57,11 +57,11 @@ import{K as k,u as h,g as S,f as y,h as j,m as $,c as w,r as q}from"../../assets
               </svg>
             </div>
             <p class="list-price__price js-current-price" 
-            data-price="${t}">
-            ${t}<span> ₴</span>
+            data-price="${n}">
+            ${n}<span> ₴</span>
             </p>
           </div>
-        </li>`})}function C(){return`
+        </li>`})}function v(){return`
       <li
         class="basket-main__list-item_empty-basket list-item__empty-basket js-empty-basket-item">
         <h3 class="list-item__empty-basket-title">
@@ -73,5 +73,5 @@ import{K as k,u as h,g as S,f as y,h as j,m as $,c as w,r as q}from"../../assets
           height="500"
           src="https://firebasestorage.googleapis.com/v0/b/flowers-4b1a5.appspot.com/o/pages%2F404%2Fempty-shopping-cart.webp?alt=media&token=3c262b31-1194-4d5e-9cc7-4c1f827f99a0"
           alt="#" />
-      </li>`}function E(){const a=document.querySelector(".js-basket-card-list"),m=document.querySelector(".js-title-shopping-list"),r=document.querySelector(".js-order-total-price"),c=document.querySelector(".js-goods-price"),o=document.querySelector(".js-discount-price");let t=JSON.parse(localStorage.getItem(k))??[];function l(e){e.length?(a.innerHTML=P(e).join(""),m.classList.remove("basket-main__continue-shopping-list_hidden")):(a.innerHTML=C(),m.classList.add("basket-main__continue-shopping-list_hidden"))}l(t),a.addEventListener("click",u);function u({target:e,type:s}){e.classList.contains("js-remove-card")&&x(e),e.closest(".js-basket-btn-increment")?g(e):e.closest(".js-basket-btn-decrement")&&v(e)}function _(e){let s=0,n=0;e.forEach(i=>{s+=i.oldPrice,n+=i.price}),r.textContent=`${n} ₴`,c.textContent=`${s} ₴`,o.textContent=`${s-n} ₴`}_(t);function g(e){const s=e.closest(".basket-main__list-item");let n=s.querySelector(".js-basket-amount"),i=s.querySelector(".js-current-price"),b=s.querySelector(".js-old-price")?s.querySelector(".js-old-price").dataset.oldPrice:+i.dataset.price,d=+n.textContent;const p=+i.dataset.price;d>=10||(n.textContent=d+=1,i.textContent=`${p*d} ₴`,c.textContent=`${parseInt(c.textContent)+Number(b)} ₴`,r.textContent=`${parseInt(r.textContent)+p} ₴`,o.textContent=`${parseInt(o.textContent)+(Number(b)-p)} ₴`)}function v(e){const s=e.closest(".basket-main__list-item");let n=s.querySelector(".js-basket-amount"),i=s.querySelector(".js-current-price"),b=s.querySelector(".js-old-price")?s.querySelector(".js-old-price").dataset.oldPrice:+i.dataset.price,d=+n.textContent;const p=+i.dataset.price;d<=1||(n.textContent=d-=1,i.textContent=`${p*d} ₴`,c.textContent=`${parseInt(c.textContent)-Number(b)} ₴`,r.textContent=`${parseInt(r.textContent)-p} ₴`,o.textContent=`${parseInt(o.textContent)-(Number(b)-p)} ₴`)}function x(e){const s=e.closest(".basket-main__list-item"),n=s.dataset.id;s.remove(),t=t.filter(({id:i})=>i!==n),localStorage.setItem(k,JSON.stringify(t)),h(),_(t),f(),t.length||(a.innerHTML=C())}window.addEventListener("storage",f);function f(){t=JSON.parse(localStorage.getItem(k))??[],h(),l(t),_(t)}}function I(a){const m=document.querySelector(".js-basket-recommendations-swiper"),r=["Recommendations"],c=[m],o=L(a);for(let t=0;t<r.length;t++){const l=r[t],u=c[t];S(o,l,u)}}document.addEventListener("DOMContentLoaded",()=>{let a=y();j(),$(),w(),E(),h(),q(),I(a)});
+      </li>`}function I(){const r=document.querySelector(".js-basket-card-list"),u=document.querySelector(".js-title-shopping-list"),o=document.querySelector(".js-order-total-price"),c=document.querySelector(".js-goods-price"),l=document.querySelector(".js-discount-price"),n=document.querySelector(".js-remove-basket-all");console.log(n);let e=JSON.parse(localStorage.getItem(g))??[];n.addEventListener("click",t=>{localStorage.removeItem(g),e=[],k(),d(e),o.textContent="0",c.textContent="0",l.textContent="0"});function d(t){t.length?(r.innerHTML=E(t).join(""),u.classList.remove("basket-main__continue-shopping-list_hidden"),n.classList.remove("basket-main__content-remove-all_hidden")):(r.innerHTML=v(),u.classList.add("basket-main__continue-shopping-list_hidden"),setTimeout(()=>{n.classList.add("basket-main__content-remove-all_hidden")},2e3))}d(e),r.addEventListener("click",h);function h({target:t,type:s}){t.classList.contains("js-remove-card")&&S(t),t.closest(".js-basket-btn-increment")?C(t):t.closest(".js-basket-btn-decrement")&&x(t)}function _(t){let s=0,a=0;t.forEach(i=>{s+=i.oldPrice,a+=i.price}),o.textContent=`${a} ₴`,c.textContent=`${s} ₴`,l.textContent=`${s-a} ₴`}_(e);function C(t){const s=t.closest(".basket-main__list-item");let a=s.querySelector(".js-basket-amount"),i=s.querySelector(".js-current-price"),b=s.querySelector(".js-old-price")?s.querySelector(".js-old-price").dataset.oldPrice:+i.dataset.price,m=+a.textContent;const p=+i.dataset.price;m>=10||(a.textContent=m+=1,i.textContent=`${p*m} ₴`,c.textContent=`${parseInt(c.textContent)+Number(b)} ₴`,o.textContent=`${parseInt(o.textContent)+p} ₴`,l.textContent=`${parseInt(l.textContent)+(Number(b)-p)} ₴`)}function x(t){const s=t.closest(".basket-main__list-item");let a=s.querySelector(".js-basket-amount"),i=s.querySelector(".js-current-price"),b=s.querySelector(".js-old-price")?s.querySelector(".js-old-price").dataset.oldPrice:+i.dataset.price,m=+a.textContent;const p=+i.dataset.price;m<=1||(a.textContent=m-=1,i.textContent=`${p*m} ₴`,c.textContent=`${parseInt(c.textContent)-Number(b)} ₴`,o.textContent=`${parseInt(o.textContent)-p} ₴`,l.textContent=`${parseInt(l.textContent)-(Number(b)-p)} ₴`)}function S(t){const s=t.closest(".basket-main__list-item"),a=s.dataset.id;s.remove(),e=e.filter(({id:i})=>i!==a),localStorage.setItem(g,JSON.stringify(e)),k(),_(e),f(),e.length||(r.innerHTML=v())}window.addEventListener("storage",f);function f(){e=JSON.parse(localStorage.getItem(g))??[],k(),d(e),_(e)}}function N(r){const u=document.querySelector(".js-basket-recommendations-swiper"),o=["Recommendations"],c=[u],l=P(r);for(let n=0;n<o.length;n++){const e=o[n],d=c[n];y(l,e,d)}}document.addEventListener("DOMContentLoaded",()=>{let r=j();$(),q(),w(),I(),k(),L(),N(r)});
 //# sourceMappingURL=basket.js.map
